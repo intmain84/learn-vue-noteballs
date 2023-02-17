3d948e59d1df6e1719578212b91e43c6d5e439f8
intmain84 <aheleha@gmail.com>
2023-02-17 23:58:56 +0600

    dfdf

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -1,34 +1,65 @@
 <template>
   <nav class="navbar is-success" role="navigation" aria-label="main navigation">
-    <div class="navbar-brand">
-      <RouterLink to="/"><img src="@/assets/logo.png" alt="" /></RouterLink>
+    <div class="container is-max-desktop">
+      <div class="navbar-brand is-align-items-center">
+        <RouterLink to="/" class="c-navbar-item is-size-3 is-family-monospace"
+          >NOTEBALLS</RouterLink
+        >
+        <a
+          role="button"
+          class="navbar-burger"
+          :class="{ 'is-active': burgerMenuIsActive }"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+          @click.prevent="toggleBurgerMenu"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
 
-      <a
-        role="button"
-        class="navbar-burger"
-        aria-label="menu"
-        aria-expanded="false"
-        data-target="navbarBasicExample"
-      >
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-      </a>
-    </div>
-
-    <div class="navbar-menu">
-      <div class="navbar-end">
-        <RouterLink class="navbar-item" to="/">Notes</RouterLink>
-        <RouterLink class="navbar-item" to="/stats">Stats</RouterLink>
+      <div class="navbar-menu" :class="{ 'is-active': burgerMenuIsActive }">
+        <div class="navbar-end">
+          <RouterLink class="navbar-item" to="/">Notes</RouterLink>
+          <RouterLink class="navbar-item" to="/stats">Stats</RouterLink>
+        </div>
       </div>
     </div>
   </nav>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref, onBeforeUnmount } from "vue";
+
+/*
+  Burger Menu manipulation
+ */
+const burgerMenuIsActive = ref(false);
+const toggleBurgerMenu = () => {
+  burgerMenuIsActive.value = !burgerMenuIsActive.value;
+};
+onBeforeUnmount(() => {
+  burgerMenuIsActive.value = false;
+});
+
+/*
+  Something else
+ */
+</script>
 
 <style scoped>
-.navbar .navbar-brand {
-  width: 130px;
+.c-navbar-item {
+  color: #fff;
+  font-weight: 700;
+}
+
+@media (max-width: 1023px) {
+  .navbar-menu {
+    position: absolute;
+    left: 0;
+    width: 100%;
+  }
 }
 </style>
