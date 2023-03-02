7da4b8f59a10e803b0960f8a12b4ad752fea5153
intmain84 <aheleha@gmail.com>
2023-03-02 17:52:34 +0600

    CreateDB

---
+++ src/js/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDtYl9zsN7DtKS2jiKCdQgBpDXR9If8nwE",
+  authDomain: "noteballs-6b662.firebaseapp.com",
+  projectId: "noteballs-6b662",
+  storageBucket: "noteballs-6b662.appspot.com",
+  messagingSenderId: "62863962580",
+  appId: "1:62863962580:web:d84ce7230fde08c5a72641",
+};
+
+const app = initializeApp(firebaseConfig);
